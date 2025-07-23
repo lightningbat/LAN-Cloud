4ee4ef290aeb3c7ffbdc094b233e336571f85b8a
lightningbat <183808230+lightningbat@users.noreply.github.com>
2025-07-23 08:29:17 +0530

    minor UI bug fix

--- frontend/src/layout/small_ui/action_bar/index.jsx
+++ frontend/src/layout/small_ui/action_bar/index.jsx
@@ -12,45 +12,45 @@ import {
 } from "../../../icons"
 
 export default function ActionBar() {
-    const icon_style = { width: '1.2rem', height: '1.2rem' }
+    const icon_style = { width: '1.2rem', height: '1.2rem' }   
     return (
         <div className="action-bar">
-            <button className="tooltip">
-                <PasteIcon style={icon_style} />
+            <div className="tooltip">
+                <button className="tooltip-btn"><PasteIcon style={icon_style} /></button>
                 <span className="tooltip-text">Paste</span>
-            </button>
-            <button className="tooltip">
-                <CheckBoxIcon style={icon_style} />
+            </div>
+            <div className="tooltip">
+                <button className="tooltip-btn"><CheckBoxIcon style={icon_style} /></button>
                 <span className="tooltip-text">Select</span>
-            </button>
-            <button className="tooltip">
-                <CopyIcon style={icon_style} />
+            </div>
+            <div className="tooltip">
+                <button className="tooltip-btn"><CopyIcon style={icon_style} /></button>
                 <span className="tooltip-text">Copy</span>
-            </button>
-            <button className="tooltip">
-                <CutIcon style={icon_style} />
+            </div>
+            <div className="tooltip">
+                <button className="tooltip-btn"><CutIcon style={icon_style} /></button>
                 <span className="tooltip-text">Cut</span>
-            </button>
-            <button className="tooltip">
-                <DeleteIcon style={icon_style} />
+            </div>
+            <div className="tooltip">
+                <button className="tooltip-btn"><DeleteIcon style={icon_style} /></button>
                 <span className="tooltip-text">Delete</span>
-            </button>
-            <button className="tooltip">
-                <RenameIcon style={icon_style} />
+            </div>
+            <div className="tooltip">
+                <button className="tooltip-btn"><RenameIcon style={icon_style} /></button>
                 <span className="tooltip-text">Rename</span>
-            </button>
-            <button className="tooltip">
-                <AddIcon style={icon_style} />
+            </div>
+            <div className="tooltip">
+                <button className="tooltip-btn"><AddIcon style={icon_style} /></button>
                 <span className="tooltip-text">Add</span>
-            </button>
-            <button className="tooltip">
-                <TagIcon style={icon_style} />
+            </div>
+            <div className="tooltip">
+                <button className="tooltip-btn"><TagIcon style={icon_style} /></button>
                 <span className="tooltip-text">Tag</span>
-            </button>
-            <button className="tooltip">
-                <LockIcon style={icon_style} />
+            </div>
+            <div className="tooltip">
+                <button className="tooltip-btn"><LockIcon style={icon_style} /></button>
                 <span className="tooltip-text">Lock</span>
-            </button>
+            </div>
         </div>
     )
 }
